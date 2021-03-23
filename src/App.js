0423bfa9f63bf2623e1f492d108ff69270deedac
Atul Kumar <mycodingx@gmail.com>
2021-03-23 21:08:18 +0530

    using pokemon api to create observable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { useState, useEffect } from "react";
-import { from, observable } from "rxjs";
-import { map, filter, delay, mergeMap } from "rxjs/operators";
+import { BehaviorSubject, from } from "rxjs";
+import {
+  filter,
+  mergeMap,
+  debounceTime,
+  distinctUntilChanged,
+} from "rxjs/operators";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./App.css";
+import pokeball from "./assets/pokeball.png";
 
-let numbersObservable = from([1, 2, 3, 4, 5]);
+const getPokemonByName = async (name) => {
+  const { results: allPokemon } = await fetch(
+    "https://pokeapi.co/api/v2/pokemon/?limit=1000"
+  ).then((res) => res.json());
+  return allPokemon.filter((pokemon) => pokemon.name.includes(name));
+};
 
-let squaredNumbers = numbersObservable.pipe(
-  filter((val) => val > 2),
-  mergeMap((val) => from([val]).pipe(delay(1000 * val))),
-  map((val) => val * val)
+const getPokemonImage = (pokemonUrl) => {
+  return `https://pokeres.bastionbot.org/images/pokemon/${
+    pokemonUrl.split("/")[6]
+  }.png`;
+};
+
+let searchSubject = new BehaviorSubject("");
+
+let searchResultObservable = searchSubject.pipe(
+  filter((val) => val.length > 1),
+  debounceTime(750),
+  distinctUntilChanged(),
+  mergeMap((val) => from(getPokemonByName(val)))
 );
 
 const useObservable = (observable, setter) => {
@@ -21,14 +42,41 @@ const useObservable = (observable, setter) => {
 };
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState(0);
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
 
-  useObservable(squaredNumbers, setCurrentNumber);
+  useObservable(searchResultObservable, setResults);
+
+  const handleSearch = (e) => {
+    const newValue = e.target.value;
+    setSearch(newValue);
+    searchSubject.next(newValue);
+  };
 
   return (
     <div className="App">
-      <h1>Counter</h1>
-      <p>Current count is: {currentNumber}</p>
+      <h1>Search Pokemon</h1>
+      <img src={pokeball} alt="pokemon" className="pokeball" />
+      <div>
+        <input
+          type="text"
+          placeholder="Search...."
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="card-outer">
+        {results.map((pokemon) => (
+          <div key={pokemon.name} className="card">
+            <LazyLoadImage
+              alt={pokemon.name}
+              src={getPokemonImage(pokemon.url)}
+              placeholderSrc={pokeball}
+            />
+            <b>{pokemon.name.toUpperCase()}</b>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
